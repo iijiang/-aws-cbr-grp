34b2590f744fbe75a4bda640d434ab2ae1ab4dcc
Yi Jiang <yi.jiang@education.gov.au>
2025-09-13 11:21:59 +1000

    First commit

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Root route
+app.get('/', (req, res) => {
+  res.send('Hello from your Node.js demo app!');
+});
+
+// /ping route
+app.get('/ping', (req, res) => {
+  res.json({ pong: true });
+});
+
+app.listen(port, () => {
+  console.log(`Demo app listening at http://localhost:${port}`);
+});
